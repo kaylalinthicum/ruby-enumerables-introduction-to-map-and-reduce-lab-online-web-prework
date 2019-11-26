bcdc92fc3d30c0c3d6b96f0c2916e7a1ee524806
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-26 19:18:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  element_index = 0
+  while element_index < source_array.count do
+    new_array << (element_index * -1)
+  end 
+  new_array
+end 
+
+def map_to_no_change(source_array)
+  new_array = []
+  element_index = 0
+  while element_index < source_array.count do
+    new_array << element_index
+  end
+  new_array
+end 
+
+def map_to_double(source_array)
+  new_array = []
+  element_index = 0
+  while element_index < source_array.count do
+    new_array << (element_index * 2)
+  end 
+  new_array
+end 
+
+def map_to_square(source_array)
+  new_array = []
+  element_index = 0
+  while element_index < source_array.count do
+    new_array << (element_index ** 2)
+  end 
+  new_array
+end 
